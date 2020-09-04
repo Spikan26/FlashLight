6d14a212c193d084c248216156859b865006013a
Alexandre <durieuxalexandre26@gmail.com>
2020-09-04 11:42:49 +0200

    App fonctionnelle
    Après de nombreux test, j'ai pu trouvé la méthode pour ma version d'android

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -1,47 +1,66 @@
 package com.example.flashlight;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.hardware.Camera;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton switchBtnOn, switchBtnOff;
-    Camera camera;
 
+    private  CameraManager cameraManager;
+    private  String camera;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            camera = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
         switchBtnOn = findViewById(R.id.switchBtnOn);
         switchBtnOff = findViewById(R.id.switchBtnOff);
 
-        camera = Camera.open();
-        final Camera.Parameters parameters = camera.getParameters();
-
         switchBtnOn.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                 switchBtnOn.setVisibility(View.GONE);
                 switchBtnOff.setVisibility(View.VISIBLE);
-                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                camera.setParameters(parameters);
-                //camera.startPreview();
+
+                try {
+                    cameraManager.setTorchMode(camera, true);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         switchBtnOff.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                 switchBtnOff.setVisibility(View.GONE);
                 switchBtnOn.setVisibility(View.VISIBLE);
-                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                camera.setParameters(parameters);
-                camera.stopPreview();
+
+                try {
+                    cameraManager.setTorchMode(camera, false);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
